1199b5adf7572f57045ac2dbfe82a3449c35f6ee
MitchellNorthern <mitchelln01@outlook.com>
2021-11-28 19:21:04 -0600

    Remove old compiled Frostwork code  and update the project

--- dist/src/store/getStore.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getStore = void 0;
-function getStore() { }
-exports.getStore = getStore;
-//# sourceMappingURL=getStore.js.map
