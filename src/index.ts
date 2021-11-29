0ee723e39772e7309fb15da8ba8f79135f054e94
MitchellNorthern <mitchelln01@outlook.com>
2021-11-28 19:16:06 -0600

    Remove unused code from the proxies branch

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import { createStore } from './store/createStore'
-import { combineStores } from './store/combineStores'
-import { getStore } from './store/getStore'
-import {
-    Store,
-    OpticStore,
-    CombinedStore,
-    KeyAndStore
-} from './types/storeTypes'
-
-interface Frostwork {
-    readonly createStore: (obj: Store) => OpticStore
-    readonly combineStores: (keysAndStores: KeyAndStore[]) => CombinedStore
-    readonly getStore: () => void
-}
-
-const Frostwork: Frostwork = {
-    createStore,
-    combineStores,
-    getStore
-}
+interface Frostwork {}
 
 export default Frostwork
--- test/test.ts
+++
@@ -1,3 +0,0 @@
-import Frostwork from '../src/index'
-
-//Frostwork.createStore()
